af137ddd87a18d3e8e98e46c8243b33eda6e8ccc
wu2wa <2639485878@qq.com>
2020-04-25 17:25:35 +0800

    33333333

--- demo/src/com/wh/demo/Demo.java
+++ demo/src/com/wh/demo/Demo.java
@@ -2,8 +2,8 @@ package com.wh.demo;
 
 public class Demo {
     public static void main(String[] args){
-        System.out.println("aaaaaa");
-        System.out.println("ffffffggggff");
+        System.out.println("1");
+        System.out.println("2");
 
     }
 }
